fd0d09c977491bd1fbb86f41be828cf89897f886
cksgud0403 <cksgud0403@naver.com>
2022-09-14 00:38:28 +0900

    ??!

---
+++ 백준 알고리즘/p10926/Main.java
@@ -0,0 +1,16 @@
+package p10926;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String str = scanner.nextLine();
+		System.out.println(str + "??!");
+		
+		scanner.close();
+	}
+
+}
