3ada87632f0a4416542875978c5942221e73db6d
cksgud0403 <cksgud0403@naver.com>
2022-09-14 00:45:17 +0900

    나머지

---
+++ 백준 알고리즘/p10430/Main.java
@@ -0,0 +1,24 @@
+package p10430;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		
+		int b = scanner.nextInt();
+		
+		int c = scanner.nextInt();
+		
+		System.out.println((a+b) % c);
+		System.out.println(((a%b) + (b%c))%c);
+		System.out.println((a*b)%c);
+		System.out.println(((a%c) * (b%c))%c);
+		
+		scanner.close();
+	}
+
+}
