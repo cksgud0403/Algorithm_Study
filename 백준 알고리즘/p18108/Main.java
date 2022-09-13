f6b62e87391efae4e3e3e40eb8c6975c62d76394
cksgud0403 <cksgud0403@naver.com>
2022-09-14 00:40:59 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ 백준 알고리즘/p18108/Main.java
@@ -0,0 +1,16 @@
+package p18108;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		
+		System.out.println(a-543);
+		
+		scanner.close();
+	}
+
+}
