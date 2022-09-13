8141f3ec79637a389bf96ab88c29c429edc43e30
cksgud0403 <cksgud0403@naver.com>
2022-09-14 02:53:50 +0900

    윤년

---
+++ 백준 알고리즘/p2753/Main.java
@@ -0,0 +1,21 @@
+package p2753;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);	
+		
+		int year = scanner.nextInt();
+		
+		if(year % 4 == 0 && year % 100 !=0 || year % 400 == 0) {
+			System.out.println(1);
+		}else {
+			System.out.println(0);
+		}
+		
+		scanner.close();
+	}
+
+}
