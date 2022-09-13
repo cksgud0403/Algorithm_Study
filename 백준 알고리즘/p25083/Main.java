51391c996fb891aa8b0247c6f412dc4cee11c973
cksgud0403 <cksgud0403@naver.com>
2022-09-14 00:57:20 +0900

    새싹

---
+++ 백준 알고리즘/p25083/Main.java
@@ -0,0 +1,21 @@
+package p25083;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		
+		stringBuilder.append("         ,r'\"7\n");
+		stringBuilder.append("r`-_   ,'  ,/\n");
+		stringBuilder.append(" \\. \". L_r'\n");
+		stringBuilder.append("   `~\\/\n");
+		stringBuilder.append("      |\n");
+		stringBuilder.append("      |\n");
+		
+		
+		System.out.println(stringBuilder.toString());
+	}
+
+}
