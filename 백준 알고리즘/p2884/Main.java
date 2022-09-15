572a6dee97df1a56173f53534cb977f785fedb76
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2022-09-15 13:54:33 +0900

    알람 시계

---
+++ 백준 알고리즘/p2884/Main.java
@@ -0,0 +1,30 @@
+package p2884;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int H = scanner.nextInt();
+		
+		int M = scanner.nextInt();
+		
+		M -= 45;
+		
+		if(M < 0) {
+			H -= 1;
+			M += 60;
+		}
+		
+		if(H < 0) {
+			H += 24;
+		}
+		
+		System.out.println(H + " " + M);
+		scanner.close();
+
+	}
+
+}
