42b6675e32c19cbe06b65bffcd099f05df5352f0
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2022-09-20 23:35:30 +0900

    구구단

---
+++ 백준 알고리즘/p2739/Main.java
@@ -0,0 +1,21 @@
+package p2739;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		
+		for(int i = 1; i<=9; i++) {
+			System.out.println(N + " * " + i + " = " + N*i);
+		}
+		
+		scanner.close();
+
+	}
+
+}
