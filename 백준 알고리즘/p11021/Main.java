51345cf8e83d408b056f9c88428df7d4a263aef9
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2022-09-29 00:58:15 +0900

    A+B - 7

---
+++ 백준 알고리즘/p11021/Main.java
@@ -0,0 +1,29 @@
+package p11021;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i< T; i++) {
+			String[] str = br.readLine().split(" ");
+			
+			bw.write("Case #"+ (i+1) +": " + (Integer.parseInt(str[0]) + Integer.parseInt(str[1])) +"\n");
+		}
+		
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+
+}
