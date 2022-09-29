2fee49f0710630721fc2b1d97a604cf9c407811d
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2022-09-29 23:04:53 +0900

    별 찍기 - 1

---
+++ 백준 알고리즘/p2438/Main.java
@@ -0,0 +1,30 @@
+package p2438;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i<N; i++) {
+			for(int j = 0; j <= i; j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+
+}
