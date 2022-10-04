45171378c711235e9a46bf6c2edd47055fc45240
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2022-10-04 23:57:02 +0900

    아스키 코드

---
+++ 백준 알고리즘/p11654/Main.java
@@ -0,0 +1,27 @@
+package p11654;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String str = br.readLine();
+		
+		int ASCII = (int)str.charAt(0);
+		
+		bw.write(String.valueOf(ASCII));
+		
+		bw.flush();
+		
+		br.close();
+		bw.close();
+	}
+
+}
