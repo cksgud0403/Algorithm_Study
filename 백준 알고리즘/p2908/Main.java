a2c806dc737512f65fc74eff8e49bbe3a7bbe4d7
cksgud0403 <cksgud0403@naver.com>
2022-12-27 21:08:36 +0900

    상수

---
+++ 백준 알고리즘/p2908/Main.java
@@ -0,0 +1,21 @@
+package p2908;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		
+		scanner.close();
+		
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.println(A > B ? A : B);
+	}
+}
