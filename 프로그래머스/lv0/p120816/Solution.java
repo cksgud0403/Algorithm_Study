a654f5b84b2d19d75f8d45a7caefb20ffb25bca7
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2023-01-05 17:37:23 +0900

    피자 나눠 먹기 (3)

---
+++ 프로그래머스/lv0/p120816/Solution.java
@@ -0,0 +1,11 @@
+package p120816;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		System.out.println(Math.round(3.1415));
+		System.out.println(Math.round(31.415) / (double)10);
+
+	}
+
+}
