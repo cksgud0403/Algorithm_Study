3e814ca55bc457944badf31730d044ab5dbb266f
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2023-01-05 14:39:14 +0900

    중앙값 구하기

---
+++ 프로그래머스/lv0/p120811/Solution.java
@@ -0,0 +1,17 @@
+package p120811;
+
+import java.util.Arrays;
+
+public class Solution {
+	public int solution(int[] array) {
+        Arrays.sort(array);
+        
+        return array[array.length/2];
+        
+    }
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
