c377e7706b595175cc0646d963eab78c8212afa8
cksgud0403 <42197731+cksgud0403@users.noreply.github.com>
2023-01-06 22:03:03 +0900

    아이스 아메리카노

---
+++ 프로그래머스/lv0/p120819/Solution.java
@@ -0,0 +1,22 @@
+package p120819;
+
+import java.util.Arrays;
+
+public class Solution {
+	public static int[] solution(int money) {
+		
+		int[] answer = new int[2];
+		
+		answer[0] = money / 5500;
+		answer[1] = money % 5500;
+
+		return answer;
+    }
+	
+
+	public static void main(String[] args) {
+		
+		System.out.println(Arrays.toString(solution(15000)));
+	}
+
+}
