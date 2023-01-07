b0ba71aa637797ae80bebbc54406e7f7cada34b4
cksgud0403 <cksgud0403@naver.com>
2023-01-07 23:45:10 +0900

    문자열 뒤집기

---
+++ 프로그래머스/lv0/p120822/Solution.java
@@ -0,0 +1,16 @@
+package p120822;
+
+public class Solution {
+	 public String solution(String my_string) {
+	     String revers_string = new StringBuilder(my_string).reverse().toString();
+		 
+		 return revers_string;
+	 }
+	 
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
