cd5a860136fc9d8030aef30568b87b58a1e0e2cc
cksgud0403 <cksgud0403@naver.com>
2023-01-15 22:10:29 +0900

    양꼬치

---
+++ 프로그래머스/lv0/p120830/Solution.java
@@ -0,0 +1,13 @@
+package p120830;
+
+public class Solution {
+	 public int solution(int n, int k) {
+	        return  n * 12000 + k * 2000 - (n / 10) * 2000;
+	 }
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
