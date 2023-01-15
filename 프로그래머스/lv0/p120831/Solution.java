cc7ec0138d11d721ca7d79e98c991720cad46104
cksgud0403 <cksgud0403@naver.com>
2023-01-15 22:14:54 +0900

    짝수의 합

---
+++ 프로그래머스/lv0/p120831/Solution.java
@@ -0,0 +1,21 @@
+package p120831;
+
+public class Solution {
+	 public int solution(int n) {
+	        int sum = 0;
+	        
+	        for(int i = 0; i <= n; i++) {
+	            if(i % 2 == 0) {
+	                sum += i;
+	            }
+	        }
+	        return sum;
+	 }
+	 
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
