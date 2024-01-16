24ef4c0b4ffc5a82f37cf935d5c9bc93520787c7
cksgud0403 <cksgud0403@naver.com>
2024-01-16 22:20:35 +0900

    x만큼 간격이 있는 n개의 숫자

---
+++ 프로그래머스/lv1/p12954/Solution.java
@@ -0,0 +1,23 @@
+package p12954;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        long num = x;
+
+        for(int i = 0; i < n; i++) {
+            answer[i] = num;
+            num += x;
+        }
+
+        return answer;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(2, 5)));;
+    }
+}
