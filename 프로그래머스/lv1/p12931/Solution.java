61d2ded332c647d60772c965a44b227672a6198e
cksgud0403 <cksgud0403@naver.com>
2024-01-16 22:22:21 +0900

    자릿수 더하기

---
+++ 프로그래머스/lv1/p12931/Solution.java
@@ -0,0 +1,20 @@
+package p12931;
+
+public class Solution {
+    public static int solution(int n) {
+        int answer = 0;
+
+
+        while(n != 0) {
+            answer += n % 10;
+            n /= 10;
+        }
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(123));;
+    }
+}
